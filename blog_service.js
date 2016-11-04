c1162e221386499538027ebb424d8c282ba9d7df
klords <klords@asu.edu>
2016-11-04 00:51:01 -0700

    Added deletion logic including cascading deletion for EC

--- blog_service.js
+++ blog_service.js
@@ -16,6 +16,7 @@ class BlogResponder extends EventEmitter {
         this.res = null;
         this.articleList = [];
         this.fragmentList = [];
+        this.mediaSearchQueue = [];
         this.userRole = 'visitor';
         this.userName = 'Visitor';
         this.target = '';
@@ -31,7 +32,8 @@ class BlogResponder extends EventEmitter {
 
     initListeners() {
         this.on('articlesLoaded',   this.getFragments);
-        this.on('fragmentsLoaded',	this.getRequestTarget);
+        this.on('fragmentsLoaded',	this.getMedia);
+        this.on('mediaLoaded',      this.getRequestTarget);
         this.on('targetAcquired',   this.generatePayload);
         this.on('payloadLoaded',    this.generateHeader);
         this.on('responseReady',    this.sendResponse);
@@ -41,6 +43,7 @@ class BlogResponder extends EventEmitter {
         this.routeMap['./auth']             = './blogs/auth.html';
         this.routeMap['./createArticle']    = './blogs/createArticle.html';
         this.routeMap['./createFragment']   = './blogs/createFragment.html';
+        this.routeMap['./delete']           = './blogs/delete.html';
     }
 
     getArticles() {
@@ -62,9 +65,38 @@ class BlogResponder extends EventEmitter {
     	fs.readdir('./blogs', (err, files) => {
             if (err) throw err;
             this.fragmentList = files.filter((name) => {
-                return name.endsWith('frag.html');
+                return name.endsWith('.frag.html');
             });
             this.emit('fragmentsLoaded');
+        });
+    }
+
+    getMedia() {
+        this.findMediaRefs(this.mediaSearchQueue);
+    }
+
+    findMediaRefs(searchQueue) {
+        if (searchQueue.length === 0) {
+            this.emit('mediaLoaded');
+            return;
+        }
+        let searchObj = searchQueue.pop();
+        let articleObj = null;
+        for (let article of this.articleList) {
+            if (article.name === searchObj.article)
+                articleObj = article;
+        }
+        fs.readFile(`./blogs/${searchObj.file}`, 'utf8', (err, data) => {
+            if (err) {
+                this.sendError(500);
+                return;
+            }
+            let searchExp = new RegExp('src=["\']([\\w\\d\/\\\\_.]+)["\']', 'g');
+            let matches;
+            while ((matches = searchExp.exec(data)) !== null) {
+                articleObj.Media.push(matches[1]);
+            }
+            this.findMediaRefs(searchQueue);
         });
     }
 
@@ -81,6 +113,13 @@ class BlogResponder extends EventEmitter {
             }
             let articleObj = JSON.parse(data);
             articleObj.name = currentArticle;
+            articleObj.Media = [];
+            if (articleObj.Fragments) {
+                for (let fragment of articleObj.Fragments) {
+                    this.mediaSearchQueue.push({article: currentArticle,
+                                           file: fragment});
+                }
+            }
             this.articleList.push(articleObj);
             this.loadArticleData(articleNames);
         });
@@ -146,7 +185,7 @@ class BlogResponder extends EventEmitter {
         if (queryObj && queryObj.role) {
              newRole = queryObj.role.toLowerCase();
         }
-        let target = `.${targetObj.href}`;
+        let target = `.${targetObj.pathname}`;
         target = target.replace(/\.\./g, ''); // no directory traversal
 
         switch (target) {
@@ -154,15 +193,21 @@ class BlogResponder extends EventEmitter {
                 target += 'landing';
                 break;
             case './auth':
-                if (queryObj  && queryObj.username == queryObj.password && validRoles.includes(newRole)) {
-                    this.userRole = newRole;
-                    this.userName = queryObj.username;
-                    this.statusCode = 302;
-                    this.responseHeader = {
-                        'Location': '/'
-                    };
-                    this.emit('payloadLoaded');
-                    return;
+                if (queryObj) {  
+                    if (queryObj.username === queryObj.password && validRoles.includes(newRole)) {
+                        this.userRole = newRole;
+                        this.userName = queryObj.username;
+                        this.statusCode = 302;
+                        this.responseHeader = {
+                            'Location': '/'
+                        };
+                        this.emit('payloadLoaded');
+                        return;
+                    } else if (!validRoles.includes(newRole)) {
+                        this.errorMessage = "Invalid Role selected.";
+                    } else if (queryObj.username !== queryObj.password) {
+                        this.errorMessage = "Invalid username/password.";
+                    }
                 }
                 break;
             case './createFragment':
@@ -206,6 +251,18 @@ class BlogResponder extends EventEmitter {
                     return;
                 }
             	break;
+            case './delete':
+                if (this.req.requestData) { // form submission
+                    queryObj = qstring.parse(this.req.requestData);
+                    this.deleteArticles(queryObj);                    
+                    this.statusCode = 302;
+                    this.responseHeader = {
+                        'Location': '/'
+                    };
+                    this.emit('payloadLoaded');
+                    return;
+                }
+                break;
             case './quit':
                 this.userRole = 'visitor';
                 this.userName = 'Visitor';
@@ -223,14 +280,40 @@ class BlogResponder extends EventEmitter {
 
     createArticle(queryObj) {
     	let newArticleData = "{\"Title\":\"" + queryObj.title + "\",\"Author\":\"" + this.userName + "\",\"Public\":\"" + queryObj.pubpriv + "\",\"Fragments\":[";
-    	for (let x = 0; x < queryObj.frag.length; x++)  {
-    		if (x == queryObj.frag.length - 1)
-    			newArticleData += "\"" + queryObj.frag[x] + "\"";
-    		else
-    			newArticleData += "\"" + queryObj.frag[x] + "\",";
+    	if (queryObj.frag) {
+            if (queryObj.frag.constructor == ''.constructor) {
+                newArticleData += "\"" + queryObj.frag + "\"";
+            } else {
+                for (let x = 0; x < queryObj.frag.length; x++)  {
+                    if (x == queryObj.frag.length - 1)
+                        newArticleData += "\"" + queryObj.frag[x] + "\"";
+                    else
+                        newArticleData += "\"" + queryObj.frag[x] + "\",";
+                }
+            }
+    		newArticleData += "]}";
+    		return newArticleData;
     	}
     	newArticleData += "]}";
     	return newArticleData;
+    }
+
+    deleteArticles(queryObj) {
+        let toDeleteList = [`./${queryObj.del_article}`];
+        if (queryObj.del_unref) {
+            toDeleteList = toDeleteList.concat(queryObj.del_unref);
+        }
+        this.deleteFile(toDeleteList);
+    }
+
+    deleteFile(deletionList) {
+        if (deletionList.length === 0)
+            return;
+        fs.unlink(deletionList.pop(), (err) => {
+            if (err)
+                console.log(err);
+            this.deleteFile(deletionList);
+        });
     }
 
     canAccess(articleObj) {
@@ -246,7 +329,7 @@ class BlogResponder extends EventEmitter {
     canDelete(articleObj) {
         if (this.userRole === 'author' && 
             articleObj.Author === this.userName &&
-            articleObj.Public === 'yes')
+            articleObj.Public === 'no')
             return true;
         return false;
     }
@@ -315,56 +398,7 @@ class BlogResponder extends EventEmitter {
                 this.sendError(404);
                 return;    
             }
-            let tempStr = data.toString();
-            if (tempStr.includes('[*login_logout*]')) {
-                let newAnchor = "<a href='/";
-                if (this.userRole === 'visitor')
-                    newAnchor += "auth'>Auth";
-                else
-                    newAnchor += "quit'>Quit";
-                newAnchor += "</a>";
-                tempStr = tempStr.replace('[*login_logout*]', newAnchor);
-                data = Buffer.from(tempStr);
-            }
-			if (tempStr.includes('[*create_article*]')) {
-            	let newAnchor = "";
-            	if (this.userRole === 'author')
-            		newAnchor = "<a href='/createArticle'>Create Article</a>";
-                tempStr = tempStr.replace('[*create_article*]', newAnchor);
-                data = Buffer.from(tempStr);
-            }
-            if (tempStr.includes('[*user_status*]')) {
-            	let newUserStatus = "";
-            	if (this.userRole === 'visitor')
-            		newUserStatus = "Welcome, Visitor!";
-            	else
-            		newUserStatus = `Hello, ${this.userName}! You are logged in as ${this.userRole}.`;
-            	tempStr = tempStr.replace('[*user_status*]', newUserStatus);
-            	data = Buffer.from(tempStr);	
-            }
-            if (tempStr.includes('[*create_fragment*]')) {
-                let newAnchor = '';
-                if (this.userRole === 'author' && 
-                    this.target === './landing')
-                    newAnchor = "<a href='/createFragment'>Create New Fragment</a>";
-                tempStr = tempStr.replace('[*create_fragment*]', newAnchor);
-                data = Buffer.from(tempStr);
-            }
-            if (tempStr.includes('[*error_message*]')) {
-                let newError = this.errorMessage;
-                if (newError !== '')
-                    newError = `<div class="error">${this.errorMessage}</div>`;
-            	tempStr = tempStr.replace('[*error_message*]', newError);
-            	data = Buffer.from(tempStr);
-            }
-            if (tempStr.includes('[*fragment_list*]')) {
-            	let newFragList = "";
-            	for (let x of this.fragmentList) {
-            		newFragList += "<div class=\"checkbox\"><input type=\"checkbox\" name=\"frag\" value=\"" + x + "\"> " + x.split(".")[0] + "</div>";
-            	}
-            	tempStr = tempStr.replace('[*fragment_list*]', newFragList);
-            	data = Buffer.from(tempStr);
-            }
+            data = this.processSSIs(data);
             this.responseBody.push(data);
             if (this.loadQueue.length > 0) {
                 this.processLoadQueue(this.loadQueue.pop());
@@ -372,6 +406,129 @@ class BlogResponder extends EventEmitter {
                 this.emit('payloadLoaded');
             }
         });
+    }
+
+    processSSIs(data) {
+        let tempStr = data.toString();
+        if (tempStr.includes('[*login_logout*]')) {
+            let newAnchor = "<a href='/";
+            if (this.userRole === 'visitor')
+                newAnchor += "auth'>Auth";
+            else
+                newAnchor += "quit'>Quit";
+            newAnchor += "</a>";
+            tempStr = tempStr.replace('[*login_logout*]', newAnchor);
+            data = Buffer.from(tempStr);
+        }
+        if (tempStr.includes('[*create_article*]')) {
+            let newAnchor = "";
+            if (this.userRole === 'author')
+                newAnchor = "<a href='/createArticle'>Create Article</a>";
+            tempStr = tempStr.replace('[*create_article*]', newAnchor);
+            data = Buffer.from(tempStr);
+        }
+        if (tempStr.includes('[*user_status*]')) {
+            let newUserStatus = "";
+            if (this.userRole === 'visitor')
+                newUserStatus = "Welcome, Visitor!";
+            else
+                newUserStatus = `Hello, ${this.userName}! You are logged in as ${this.userRole}.`;
+            tempStr = tempStr.replace('[*user_status*]', newUserStatus);
+            data = Buffer.from(tempStr);	
+        }
+        if (tempStr.includes('[*create_fragment*]')) {
+            let newAnchor = '';
+            if (this.userRole === 'author' && 
+                this.target === './landing')
+                newAnchor = "<a href='/createFragment'>Create New Fragment</a>";
+            tempStr = tempStr.replace('[*create_fragment*]', newAnchor);
+            data = Buffer.from(tempStr);
+        }
+        if (tempStr.includes('[*error_message*]')) {
+            let newError = this.errorMessage;
+            if (newError !== '')
+                newError = `<div class="error">${this.errorMessage}</div>`;
+            tempStr = tempStr.replace('[*error_message*]', newError);
+            data = Buffer.from(tempStr);
+        }
+        if (tempStr.includes('[*fragment_list*]')) {
+            let newFragList = "";
+            for (let x of this.fragmentList) {
+                newFragList += "<div class=\"checkbox\"><input type=\"checkbox\" name=\"frag\" value=\"" + x + "\"> " + x.split(".")[0] + "</div>";
+            }
+            tempStr = tempStr.replace('[*fragment_list*]', newFragList);
+            data = Buffer.from(tempStr);
+        }
+        let testReg = new RegExp('\\[\\*del_');
+        if (testReg.test(tempStr)) {
+            let deletionArticle = '';
+            let urlObj = url.parse(this.req.url);
+            if (urlObj.query) {
+                let queryObj = qstring.parse(urlObj.query);
+                if (queryObj.del_name && queryObj.del_name.endsWith('.art'))
+                    deletionArticle = queryObj.del_name;
+            }
+            if (tempStr.includes('[*del_article*]')) {
+                tempStr = tempStr.replace('[*del_article*]', deletionArticle);
+            }
+            if (tempStr.includes('[*del_article_name*]')) {
+                tempStr = tempStr.replace('[*del_article_name*]', deletionArticle.replace('.art', ''));
+            }
+            if (tempStr.includes('[*del_associations*]')) {
+                let unreferencedFiles = this.getUnreferencedFiles(deletionArticle);
+                let fileSelectionList = '';
+                if (unreferencedFiles.length > 0) {
+                    fileSelectionList = '<div><p>The following files are not referenced by other applications and may be safely deleted if desired:</p>';
+                    for (let file of unreferencedFiles) {
+                        fileSelectionList += "<div class=\"checkbox\"><input type=\"checkbox\" name=\"del_unref\" value=\"" + file + "\"> " + file + "</div>"; 
+                    }
+                    fileSelectionList += '</div>';
+                }
+                tempStr = tempStr.replace('[*del_associations*]', fileSelectionList);
+            }
+            data = Buffer.from(tempStr);
+        }
+        return data;
+    }
+
+    getUnreferencedFiles(targetArticle) {
+        let unrefFiles = [];
+        let found = false;
+        let articleObj = null;
+        for (let article of this.articleList)
+            if (article.name === targetArticle) {
+                articleObj = article;
+                break;
+            }
+        if (!articleObj) // article doesn't exist
+            return unrefFiles;
+        if (articleObj.Fragments) {
+            for (let fragment of articleObj.Fragments) {
+                found = false;
+                for (let article of this.articleList) {
+                    if (article.name !== articleObj.name &&
+                        article.Fragments &&
+                        article.Fragments.includes(fragment))
+                        found = true;
+                }
+                if (!found)
+                    unrefFiles.push(`./blogs/${fragment}`);
+            }
+        }
+        if (articleObj.Media) {
+            for (let media of articleObj.Media) {
+                found = false;
+                for (let article of this.articleList) {
+                    if (article.name !== articleObj.name &&
+                        article.Media &&
+                        article.Media.includes(media))
+                        found = true;
+                }
+                if (!found)
+                    unrefFiles.push(`./${media}`);
+            }
+        }
+        return unrefFiles;
     }
 
     loadArticleLinks(article) {
